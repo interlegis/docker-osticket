34b9834e5827acd3168d13644387019cc5d4667c
matiasvictor <matias.victor@hotmail.com>
2016-12-21 11:02:40 -0200

    Memcached

--- bin/install.php
+++ bin/install.php
@@ -196,7 +196,7 @@ $usememcache = (boolean)getenv("USE_MEMCACHE");
 if ($usememcache) {
 
 $configFile= str_replace("# define('SESSION_BACKEND', 'memcache');","define('SESSION_BACKEND', 'memcache');", $configFile);
-$configFile= str_replace("# define('MEMCACHE_SERVERS', 'server1:11211,server2:11211');", "define('MEMCACHE_SERVERS', 'server1:11211');", $configFile);
+$configFile= str_replace("# define('MEMCACHE_SERVERS', 'server1:11211,server2:11211');", "define('MEMCACHE_SERVERS', 'server1:11212');", $configFile);
 }
 if (!file_put_contents($installer->getConfigFile(), $configFile)) {
    err("Failed to write configuration file");
